719d8e773d71a9cf0ac8433c77b1dbb9b6bb6ace
numbp <o9o73286699@gmail.com>
2019-08-27 12:06:51 +0900

    rand number & standerd inputでgame化

--- src/rand&stdin.rs
+++ src/rand&stdin.rs
@@ -1,5 +1,6 @@
 extern crate rand;
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
@@ -8,17 +9,29 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1, 101);
     println!("The secret number is: {}", secret_number);
 
+    loop {
+        // 標準入出力
+        println!("Please input your number.");
+        let mut input_string_2number = String::new();
 
-    // 標準入出力
-    println!("Please input your guess.");
-    let mut guess01 = String::new();
-    let mut guess02 = String::new();
+        // 入力を得る
+        io::stdin().read_line(&mut input_string_2number)
+            .expect("Failed to read line");
 
-    io::stdin().read_line(&mut guess01)
-        .expect("Failed to read line");
+        // stringの両端空白/タブをtrimしparse
+        let input_string_2number: u32 = input_string_2number.trim().parse()
+            .expect("Failed to read string to number.");
 
-    io::stdin().read_line(&mut guess02)
-        .expect("Failed to read line");
+        // input_string_2numberを受取り比較対象型にしプリント
+        match input_string_2number.cmp(&secret_number) {
+            Ordering::Less    => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal   => {
+                println!("You win!");
+                break
+            }
+        }
+    }
 
-    println!("You guessed: {} - {}", guess01 , guess02);
+    println!("game is done!!")
 }
